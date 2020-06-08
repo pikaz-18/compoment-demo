9f4eb1360ad0f9b1dfd196ca74d804e6263eca2c
zouzheng <qq2363994579@outlook.com>
2020-06-08 18:09:53 +0800

    'commit'

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: zouzheng
+ * @Date: 2020-05-08 14:06:58
+ * @LastEditors: zouzheng
+ * @LastEditTime: 2020-06-02 18:21:47
+ * @Description: 这是XXX组件（页面）
+ */
+module.exports = {
+  publicPath: "./",
+  productionSourceMap: false,
+  css: { extract: false }
+};
