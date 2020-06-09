259167e113cc90c9493346d6c8a2620e5287820f
zouzheng <qq2363994579@outlook.com>
2020-06-09 10:08:52 +0800

    'commit'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-/*Tue Jun 09 2020 10:05:26 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
+/*Tue Jun 09 2020 10:08:42 GMT+0800 (GMT+08:00)*/import ExcelCom from './components/ExcelCom';import IframeCom from './components/IframeCom';export { ExcelCom,IframeCom }; 
